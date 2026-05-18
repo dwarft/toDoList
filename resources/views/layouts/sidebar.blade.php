dc2d2ce575b73841aef90096c0f72153e6140864
cr7zone <wardengrand764@gmail.com>
2026-05-18 21:54:35 +0800

    buat page page

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,11 @@
+
+
+<div class="sidebar bg-linear-to-b from-[#200533] to-[#400C48] h-screen w-[13em] px-4 py-4">
+  <h2 class="font-bold text-white text-xl">Task<span class="text-amber-500 ">er</span></h2>
+  <ul class="text-sm text-white mt-4">
+    <li class="mt-2 p-2 rounded-sm hover:bg-[#4d456a] transition-all ease-in-out cursor-pointer">Dashboard</li>
+    <li class="mt-2 p-2 rounded-sm hover:bg-[#4d456a] transition-all ease-in-out cursor-pointer">My Task</li>
+    <li class="mt-2 p-2 rounded-sm hover:bg-[#4d456a] transition-all ease-in-out cursor-pointer">Today</li>
+    <li class="mt-2 p-2 rounded-sm hover:bg-[#4d456a] transition-all ease-in-out cursor-pointer">Important</li>
+  </ul>
+</div>
---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,14 @@
+
+@extends('master')
+@section('content')
+  <p>Dashboard</p>
+  <div class="cards">
+    {{-- <div class="card  p-4 rounded-2xl text-white bg-linear-to-br from-[#400C48] to-[#763178]"> --}}
+    <div class="card p-4 rounded-2xl text-white bg-linear-to-br from-[#400C48] to-[#763178]">
+      <div class="description">
+        <p>Task Management</p>
+        <h2 class="text-3xl font-bold w-[10em]">Manage Your Task To Better Life</h2>
+      </div>
+    </div>
+  </div>
+@endsection
